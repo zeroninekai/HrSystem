aff94954cb0499c31d1510f147d2aa98dcba034a
JC Eusebio <kai.zeronine@gmail.com>
2014-09-26 10:52:47 +0800

    Search for allocation table with wildcard fixed

--- src/main/java/novare/com/hk/repository/AllocationRepository.java
+++ src/main/java/novare/com/hk/repository/AllocationRepository.java
@@ -7,16 +7,17 @@ import novare.com.hk.model.Allocation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AllocationRepository extends JpaRepository<Allocation, Integer> {
 
 	@Query("SELECT a FROM Allocation a WHERE project.project_name = ?1")
 	public List<Allocation> filterAllocation(String filterStat);
 	
-	@Query("SELECT a FROM Allocation a WHERE LOWER(project.project_name) LIKE ?1"
-			+ " OR LOWER(employee.fname) LIKE ?1"
-			+ " OR LOWER(employee.lname) LIKE ?1")
-	public List<Allocation> searchAllocation(String search_param);
+	@Query("SELECT a FROM Allocation a WHERE LOWER(project.project_name) LIKE CONCAT('%',:search_param,'%')"
+			+ " OR LOWER(employee.fname) LIKE CONCAT('%',:search_param,'%')"
+			+ " OR LOWER(employee.lname) LIKE CONCAT('%',:search_param,'%')")
+	public List<Allocation> searchAllocation(@Param("search_param") String search_param);
 	
 	@Query("SELECT a FROM Allocation a"
 			+ " JOIN a.employee AS emp"
