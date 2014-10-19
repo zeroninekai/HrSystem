b8ee0846a5b77aabb06969be695561d79c4a8f5e
JC Eusebio <kai.zeronine@gmail.com>
2014-10-19 15:01:38 +0800

    Fixed bug for allocation repository

--- src/main/java/novare/com/hk/repository/AllocationRepository.java
+++ src/main/java/novare/com/hk/repository/AllocationRepository.java
@@ -11,12 +11,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface AllocationRepository extends JpaRepository<Allocation, Integer> {
 
-	@Query("SELECT a FROM Allocation a WHERE Project.project_name = ?1")
+	@Query("SELECT a FROM Allocation a WHERE project.project_name = ?1")
 	public List<Allocation> filterAllocation(String filterStat);
 
-	@Query("SELECT a FROM Allocation a WHERE LOWER(Project.project_name) LIKE CONCAT('%',:search_param,'%')"
-			+ " OR LOWER(Employee.fname) LIKE CONCAT('%',:search_param,'%')"
-			+ " OR LOWER(Employee.lname) LIKE CONCAT('%',:search_param,'%')")
+	@Query("SELECT a FROM Allocation a WHERE LOWER(project.project_name) LIKE CONCAT('%',:search_param,'%')"
+			+ " OR LOWER(employee.fname) LIKE CONCAT('%',:search_param,'%')"
+			+ " OR LOWER(employee.lname) LIKE CONCAT('%',:search_param,'%')")
 	public List<Allocation> searchAllocation(@Param("search_param") String search_param);
 
 	@Query(value="SELECT p.project_name AS project_name, " +
